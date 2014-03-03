aa1b0b168a6216e4929c4377a0b47f308dd5b35e
Chinmay Pendharkar <notthetup@gmail.com>
2014-03-03 14:44:35 +0800

    fixed env

--- fm.js
+++ fm.js
@@ -2,49 +2,40 @@ console.log("hello");
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-function fmSynth(audioContext, carrierFrequency, modulatorFrequency, modGain){
-  // this.modulator.frequency
-  // this.carrier.frequency
+function fmSynth(audioContext, carrier, modulator, modGain){
   // this.modulatorGain.gain
 
-  this.carrier = audioContext.createOscillator();
-  this.modulator = audioContext.createOscillator();
+  if (!carrier.hasOwnProperty('frequency')){
+    throw {
+      name: "Carrier has no frequency property",
+      message: "Attempt to access an inexistant 'frequency' property of the carrier AudioNode " + carrier,
+      toString: function () {
+        return this.name + ": " + this.message;
+      }
+    }
+  }
+
+  carrier = carrier || audioContext.createOscillator();
+  modulator = modulator || audioContext.createOscillator();
   this.modulatorGain = audioContext.createGainNode();
 
-  this.carrier.frequency.value = carrierFrequency || 400;
-  this.modulator.frequency.value = modulatorFrequency || 500;
   this.modulatorGain.gain.value = modGain || 300;
 
-
-  this.modulator.connect(this.modulatorGain);
-  this.modulatorGain.connect(this.carrier.frequency);
-  //this.carrier.connect(audioContext.destination);
+  modulator.connect(this.modulatorGain);
+  this.modulatorGain.connect(carrier.frequency);
 
   this.connect = function(audioNode){
-    this.carrier.connect(audioNode);
-  };
-
-  this.start = function(time){
-    this.modulator.start(time);
-    this.carrier.start(time);
-  };
-
-  this.stop = function(time){
-    this.modulator.stop(time);
-    this.carrier.stop(time);
+    carrier.connect(audioNode);
   };
 }
 
 function amSynth(audioContext, node1, node2){
 
-this.node1 = node1;
-this.node2 = node2;
+  var amGain = audioContext.createGain();
 
-var amGain = audioContext.createGain();
-
-this.node1.connect(amGain);
-amGain.connect(audioContext.destination);
-this.node2.connect(amGain.gain);
+  node1.connect(amGain);
+  amGain.connect(audioContext.destination);
+  node2.connect(amGain.gain);
 
   this.connect = function(audioNode){
     amGain.connect(audioNode);
@@ -58,66 +49,91 @@ function paramEAD(audioContext, attackTimeT60, decayTimeT60, param, min, max){
   /* 0.001 - 60dB Drop
       e(-n) = 0.001; - Decay Rate of setTargetAtTime.
       n = 6.90776;
-  */
+      */
 
-  this.attackTimeT60 = attackTimeT60 || 0.9;
-  this.decayTimeT60 = decayTimeT60 || 0.9;
+      this.attackTimeT60 = attackTimeT60 || 0.9;
+      this.decayTimeT60 = decayTimeT60 || 0.9;
 
-  this.min = min || 0;
-  this.min = max || 3000;
+      this.min = min || 0;
+      this.max = max || 3000;
 
-  var t60multiplier = 6.90776;
+      var t60multiplier = 6.90776;
 
-  this.trigger = function(){
-    param.cancelScheduledValues(audioContext.currentTime);
-    param.setTargetAtTime(this.max, audioContext.currentTime, this.attackTimeT60);
-    param.setTargetAtTime(this.min, audioContext.currentTime+(this.attackTimeT60/6.90776), this.decayTimeT60);
-  };
-}
+      this.trigger = function(){
+        var value = param.value;
+        param.cancelScheduledValues(audioContext.currentTime);
+        param.setValueAtTime(this.min,audioContext.currentTime);
+        param.setTargetAtTime(this.max, audioContext.currentTime, this.attackTimeT60);
+        param.setTargetAtTime(this.min, audioContext.currentTime+(this.attackTimeT60/6.90776), this.decayTimeT60);
+      };
+    }
 
 
 
-var audioContext = new AudioContext();
-var fm = new fmSynth(audioContext);
-var env = new paramEAD(audioContext, 0.9, 0.9, fm.modulator.frequency, 0, 3000);
+    var audioContext = new AudioContext();
 
-var fm = new fmSynth(audioContext);
-var osOsc = audioContext.createOscillator();
-var osOscGain = audioContext.createGain();
-osOsc.connect(osOscGain);
-var am = new amSynth(audioContext, fm, osOscGain);
+    //carrierOsc.frequency
+    //modOsc.frequency
+    //amOsc.frequency
 
-// osOsc.freqeuency
-// osOscGain.gain
+    //modOscGain.gain
+    //amOscGain.gain
 
-window.addEventListener("load",  function(){
-  var playButton=document.getElementById("playButton");
-  var carrierSlider=document.getElementById("carrierSlider");
-  var modSlider=document.getElementById("modulatorSlider");
-  var gainSlider=document.getElementById("modGain");
-  var trigButton=document.getElementById("triggerButton");
 
-  playButton.addEventListener('click', function (){
-    fm.start(0);
-    osOsc.start(0);
-  });
+    var carrierOsc = audioContext.createOscillator();
+    var modOsc = audioContext.createOscillator();
+    var amOsc = audioContext.createOscillator();
 
-  carrierSlider.addEventListener('change', function(){
-    fm.carrier.frequency.value = carrierSlider.value;
-  });
+    var modOscGain = audioContext.createGain();
+    var amOscGain = audioContext.createGain();
 
-  modSlider.addEventListener('change', function(){
-    fm.modulator.frequency.value = modSlider.value;
-  });
+    modOsc.connect(modOscGain);
+    amOsc.connect(amOscGain);
 
-  gainSlider.addEventListener('change', function(){
-    osOsc.frequency.value = gainSlider.value;
-  });
+    var carrierEnv = new paramEAD(audioContext, 0.9, 0.9, carrierOsc.frequency, 300, 3000);
+    var modEnv = new paramEAD(audioContext, 0.9, 0.9, modOsc.frequency, 500, 3000);
+    var amEvn = new paramEAD(audioContext, 0.9, 0.9, amOsc.frequency, 100, 3000);
 
-  trigButton.addEventListener('click', function (){
-    env.trigger();
-  });
+    var fm = new fmSynth(audioContext, carrierOsc, modOscGain);
+    var am = new amSynth(audioContext, fm, amOscGain);
+    am.connect(audioContext.destination);
 
-});
+    window.setInterval(function (){
+      console.log(carrierOsc.frequency.value);
+    }, 200);
 
-console.log("world");
+
+    window.addEventListener("load",  function(){
+      var playButton=document.getElementById("playButton");
+      var carrierSlider=document.getElementById("carrierSlider");
+      var modSlider=document.getElementById("modulatorSlider");
+      var gainSlider=document.getElementById("modGain");
+      var trigButton=document.getElementById("triggerButton");
+
+      playButton.addEventListener('click', function (){
+        carrierOsc.start(0);
+        modOsc.start(0);
+        amOsc.start(0);
+      });
+
+      carrierSlider.addEventListener('change', function(){
+        carrierOsc.frequency.value = carrierSlider.value;
+      });
+
+      modSlider.addEventListener('change', function(){
+        modOsc.frequency.value = modSlider.value;
+      });
+
+      gainSlider.addEventListener('change', function(){
+        amOsc.frequency.value = gainSlider.value;
+      });
+
+      trigButton.addEventListener('click', function (){
+        carrierEnv.trigger();
+        modEnv.trigger();
+        amEvn.trigger();
+      });
+
+    });
+
+    console.log("world");
